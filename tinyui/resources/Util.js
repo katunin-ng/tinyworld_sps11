8f1e4db420470c233e13fda45619e8be49a52ffc
DEVX_TECH_USER <DEVX_TECH_USER@unknown>
2016-02-23 21:44:55 +0000

    Authentication

--- tinyui/resources/Util.js
+++ tinyui/resources/Util.js
@@ -58,7 +58,6 @@ function openFirstDialog() {
 
 					},
 					error: function(data) {
-						oFirstDialog.close();
 						var message = JSON.stringify(data);
 						alert(message);
 
@@ -71,3 +70,23 @@ function openFirstDialog() {
 		oFirstDialog.open();
 	}
 }
+
+$(function() {
+	// one time fetch of CSRF token
+	$.ajax({
+		type: "GET",
+		url: "/",
+		headers: {"X-Csrf-Token": "Fetch"},
+		success: function(res, status, xhr) {
+			var sHeaderCsrfToken = "X-Csrf-Token";
+			var sCsrfToken = xhr.getResponseHeader(sHeaderCsrfToken);
+			// for POST, PUT, and DELETE requests, add the CSRF token to the header
+			$(document).ajaxSend(function(event, jqxhr, settings){
+			   if (settings.type === "POST" || settings.type === "PUT" || settings.type === "DELETE") {
+					jqxhr.setRequestHeader(sHeaderCsrfToken, sCsrfToken);
+			   }
+			});
+		}
+	});
+});
+
